948584d72af911ad7d9b8b86e255c86b1234b46f
Nick Quinn <nickquinn3131@gmail.com>
2020-04-12 14:21:04 -0400

    Finished Section 3

--- Haskell/Learning-Haskell-Programming-Udemy/Word-Game/Spec.hs
+++ Haskell/Learning-Haskell-Programming-Udemy/Word-Game/Spec.hs
@@ -13,3 +13,16 @@ main = hspec $ do
     describe "formatGrid" $ do
         it "Should concatenate every line with a newline" $ do
             (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+
+    describe "findword" $ do
+        it "Should find words that exist on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PERL" `shouldBe` Just "PERL"
+        it "Should not find words that do not exist on the Grid" $ do
+            findWord grid "DOESNOTEXIST" `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "Should find all the words that exist on the Grid" $ do
+            findWords grid languages `shouldBe` languages
+        it "Should not finall words that do not exist on the Grid" $ do
+            findWords grid ["DOES", "NOT", "EXIST"] `shouldBe` []
