1857b9bd6dad60ebe77f5bec2fd48464cd382a51
Nick Quinn <nickquinn3131@gmail.com>
2020-04-25 09:08:46 -0400

    Created type synonyms and seperated each item into it's own list

--- Haskell/Witcher-IGN-Code-Foo/witcher.hs
+++ Haskell/Witcher-IGN-Code-Foo/witcher.hs
@@ -6,6 +6,38 @@
 ----------------------------------------------------------------------------
 
 import System.IO
+import Data.List (lines)
+import Data.List.Split
+
+---------------------------- Classes and Data Types ------------------------
+--
+-- Data type that holds information about each item
+--
+
+data Item = Item {
+    armorType :: String,
+    name :: String,
+    cost :: Int,
+    armorValue :: Int
+}
+
+--
+-- Type synonym's for each type of armor to make code more readable
+--
+
+type Everything = [Item]
+type Chest = [Item]
+type Leggings = [Item]
+type Helmet = [Item]
+type Boots = [Item]
+
+---------------------------- Helper Functions ------------------------------
+--
+-- Splits each comma seperated value into it's own [String]. This means every [String]  
+-- represents a specific item in the armory
+--
+parseComma :: [String] -> [[String]]
+parseComma p = map (splitOn ",") p
 
 ---------------------------- Main Driver -----------------------------------
 --
@@ -15,4 +47,7 @@ main :: IO()
 main = do
     contents <- readFile "witcher-inventory.csv"
 
+    -- Remove header from CSV and convert contents into [String]
+    let armory = (parseComma . tail . lines) contents
+    print armory
     putStrLn "Program Finished!"
